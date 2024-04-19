4fc542f2177b89650ea5029cc447c1913abb3c34
Samiul Islam <samiulislamsharan@gmail.com>
2024-04-20 00:20:49 +0600

    Include autoloader and add user methods

--- src/endpoints/User.php
+++ src/endpoints/User.php
@@ -1,8 +1,27 @@
 <?php
+
+namespace PH7\ApiSimpleMenu;
+
 class User
 {
     public function __construct()
     {
         echo 'User class';
+    }
+
+    public function create(): void
+    {
+    }
+
+    public function retrieveAll(): array
+    {
+    }
+
+    public function retrieve(): User
+    {
+    }
+
+    public function remove(bool $enable): bool
+    {
     }
 }
